990c0258ca2fa45fc5f8ccb709fc85d41b204c05
O'Toole, James <james.otoole@ubc.ca>
2019-03-17 19:27:03 -0700

    Add responsive variants

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-module.exports = function (variants = ['responsive']) {
+module.exports = function (variants) {
   return function ({ addUtilities }) {
     addUtilities({
       '.order--1': { order: '-1' },
       '.order-0': { order: '0' },
       '.order-1': { order: '1' },
-    }, variants)
+    }, variants = ['responsive'])
   }
 }
